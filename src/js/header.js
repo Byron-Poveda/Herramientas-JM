e7db492e04943c7b17ec26414c269db14891d0d0
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-05-22 01:39:53 -0500

    Update Search Responsive

--- src/js/header.js
+++ src/js/header.js
@@ -11,7 +11,12 @@ const menuToggle = document.querySelector(`.js-open-menu`),
   mobileArrowInfo = document.querySelector('.mobile-nav__info-arrow'),
   itemSearch = document.querySelector('.header-box__item--search'),
   linkSearch = document.querySelector('.header-box__link-i--search'),
-  inputSearch = document.querySelector('.header-box__input-search');
+  inputSearch = document.querySelector('.header-box__input-search'),
+  itemSearchMobile = document.querySelector('.header__item--search'),
+  linkSearchMobile = document.querySelector('.header__link-i'),
+  inputSearchMobile = document.querySelector('.header__input-search');
+let clientWidthInput = 0;
+let clientWidthInputMobile = 0;
 //Listener para abrir el menu desplegable
 menuToggle.addEventListener('click', () => {
   menuNav.classList.toggle('is-openX');
@@ -45,12 +50,31 @@ LogoHeaderHover.addEventListener('mouseleave', () => {
   LogoHeader.classList.toggle('hide-img');
   LogoHeaderHover.classList.toggle('hide-img');
 });
-itemSearch.addEventListener('mouseover', () => {
-  inputSearch.style.width = '280px';
-  linkSearch.style.background =
-    'linear-gradient(to left, #26d1f3,#bfedf7, #e4eef0)';
-});
-itemSearch.addEventListener('mouseout', () => {
+linkSearch.addEventListener('click', () => {
+  if (clientWidthInput === 0) {
+    clientWidthInput += 1;
+    inputSearch.style.width = '280px';
+    linkSearch.style.background =
+      'linear-gradient(to left, #26d1f3,#bfedf7, #e4eef0)';
+    return;
+  }
+  clientWidthInput = 0;
   inputSearch.style.width = '0px';
   linkSearch.style.background = '#fff';
+});
+// linkSearch.addEventListener('click', () => {
+//   inputSearch.style.width = '0px';
+//   linkSearch.style.background = '#fff';
+// });
+linkSearchMobile.addEventListener('click', () => {
+  if (clientWidthInputMobile === 0) {
+    clientWidthInputMobile += 1;
+    inputSearchMobile.style.width = '100%';
+    linkSearchMobile.style.background =
+      'linear-gradient(to left, #26d1f3,#bfedf7, #e4eef0)';
+    return;
+  }
+  clientWidthInputMobile = 0;
+  inputSearchMobile.style.width = '0px';
+  linkSearchMobile.style.background = '#fff';
 });
